9aed72df9367b57e34470e8aa8170a1b0f381f9f
Ankur Bansal <ankurbnsl8099@gmail.com>
2019-09-26 19:10:30 -0400

    Creating a failing unit test

--- python/CheckoutKata/TestCheckout.py
+++ python/CheckoutKata/TestCheckout.py
@@ -14,3 +14,9 @@ def test_canAddItemPrice(checkout):
 
 def test_canAddItem(checkout):
     checkout.addItem("a")
+
+
+def test_canCalculateTotal(checkout):
+    checkout.addItemPrice("a",1)
+    checkout.addItem("a")
+    assert checkout.calculateTotal() == 1
