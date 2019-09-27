66ad28e7ab77910c8e6032e59b3011ae38caff20
Ankur Bansal <ankurbnsl8099@gmail.com>
2019-09-26 23:30:27 -0400

    passing a unit test

--- python/UnitMockTest/LineReader.py
+++ python/UnitMockTest/LineReader.py
@@ -2,6 +2,8 @@ import os
 
 
 def readFromFile(filename):
+    if not os.path.exists(filename):
+        raise Exception("Bad file")
     infile = open(filename, "r")
     line = infile.readline()
     return line
--- python/UnitMockTest/TestDouble_test.py
+++ python/UnitMockTest/TestDouble_test.py
@@ -9,6 +9,8 @@ def test_returnsCorrectString(monkeypatch):
     mock_file.readline = MagicMock(return_value="test line")
     mock_open = MagicMock(return_value=mock_file)
     monkeypatch.setattr("builtins.open", mock_open)
+    mock_exists = MagicMock(return_value=True)
+    monkeypatch.setattr("os.path.exists", mock_exists)
     result = readFromFile("check")
     mock_open.assert_called_once_with("check", "r")
     assert result == "test line"
@@ -20,6 +22,7 @@ def test_throwsExceptionWithBadFile(monkeypatch):
     mock_open = MagicMock(return_value=mock_file)
     monkeypatch.setattr("builtins.open", mock_open)
     mock_exists = MagicMock(return_value=False)
-    monkeypatch.setattr("os.path.exists",mock_exists)
+    monkeypatch.setattr("os.path.exists", mock_exists)
     with raises(Exception):
         result = readFromFile("check")
+
