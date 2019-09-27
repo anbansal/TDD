45499638eaecbd6c569c78148c355ab94e7b9fe6
Ankur Bansal <ankurbnsl8099@gmail.com>
2019-09-26 21:55:03 -0400

    Refactoring the code

--- python/CheckoutKata/Checkout.py
+++ python/CheckoutKata/Checkout.py
@@ -18,15 +18,26 @@ class Checkout:
         else:
             self.items[item] = 1
 
+    def calculateItemDiscountTotal(self, discount, item, cnt):
+        total = ((cnt / discount.nbrItems) * discount.price) + ((cnt % discount.nbrItems) * self.prices[item])
+        return total
+
+    def calculateItemTotal(self, item, cnt):
+        total = 0
+        if item in self.discount:
+            discount = self.discount[item]
+            if cnt >= discount.nbrItems:
+                total += self.calculateItemDiscountTotal(discount, item, cnt)
+            else:
+                total += self.prices[item] * cnt
+        else:
+            total += self.prices[item] * cnt
+        return total
+
     def calculateTotal(self):
         total = 0
         for item, cnt in self.items.items():
-            if item in self.discount:
-                discount = self.discount[item]
-                if cnt >= discount.nbrItems:
-                    total += ((cnt / discount.nbrItems) * discount.price) + ((cnt % discount.nbrItems) * self.prices[item])
-                    continue
-            total += self.prices[item] * cnt
+            total += self.calculateItemTotal(item, cnt)
         return total
 
     def addDiscountRule(self, item, noOfItems, newPrice):
