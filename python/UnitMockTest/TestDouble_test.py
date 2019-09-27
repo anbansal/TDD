51e26245dbe5fea9c7cb073c38eb69b49b0ff019
Ankur Bansal <ankurbnsl8099@gmail.com>
2019-09-26 23:32:26 -0400

    refactoring code

--- python/UnitMockTest/TestDouble_test.py
+++ python/UnitMockTest/TestDouble_test.py
@@ -4,11 +4,16 @@ from unittest.mock import MagicMock
 from LineReader import readFromFile
 
 
-def test_returnsCorrectString(monkeypatch):
+@pytest.fixture()
+def mock_open(monkeypatch):
     mock_file = MagicMock()
     mock_file.readline = MagicMock(return_value="test line")
     mock_open = MagicMock(return_value=mock_file)
     monkeypatch.setattr("builtins.open", mock_open)
+    return mock_open
+
+
+def test_returnsCorrectString(mock_open, monkeypatch):
     mock_exists = MagicMock(return_value=True)
     monkeypatch.setattr("os.path.exists", mock_exists)
     result = readFromFile("check")
@@ -16,13 +21,8 @@ def test_returnsCorrectString(monkeypatch):
     assert result == "test line"
 
 
-def test_throwsExceptionWithBadFile(monkeypatch):
-    mock_file = MagicMock()
-    mock_file.readline = MagicMock(return_value="test line")
-    mock_open = MagicMock(return_value=mock_file)
-    monkeypatch.setattr("builtins.open", mock_open)
+def test_throwsExceptionWithBadFile(mock_open, monkeypatch):
     mock_exists = MagicMock(return_value=False)
     monkeypatch.setattr("os.path.exists", mock_exists)
     with raises(Exception):
         result = readFromFile("check")
-
