5d56a781c8aa560a622e8725c30ec247c4f628f6
Ankur Bansal <ankurbnsl8099@gmail.com>
2019-09-26 20:26:24 -0400

    Refactor the code

--- python/CheckoutKata/TestCheckout.py
+++ python/CheckoutKata/TestCheckout.py
@@ -5,6 +5,8 @@ from Checkout import Checkout
 @pytest.fixture()
 def checkout():
     checkout = Checkout()
+    checkout.addItemPrice("a", 1)
+    checkout.addItemPrice("b", 2)
     return checkout
 
 
@@ -22,14 +24,11 @@ def test_canAddItem(checkout):
 
 
 def test_canCalculateTotal(checkout):
-    checkout.addItemPrice("a", 1)
     checkout.addItem("a")
     assert checkout.calculateTotal() == 1
 
 
 def test_canCorrectTotalMultipleItems(checkout):
-    checkout.addItemPrice("a", 1)
-    checkout.addItemPrice("b", 2)
     checkout.addItem("a")
     checkout.addItem("b")
     assert checkout.calculateTotal() == 3
