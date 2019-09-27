0e5b5e05a6718bc4ce2c2bc2454fb0cdb47039a0
Ankur Bansal <ankurbnsl8099@gmail.com>
2019-09-26 23:25:41 -0400

    adding a failing unit test

--- python/UnitMockTest/TestDouble_test.py
+++ python/UnitMockTest/TestDouble_test.py
@@ -12,3 +12,14 @@ def test_returnsCorrectString(monkeypatch):
     result = readFromFile("check")
     mock_open.assert_called_once_with("check", "r")
     assert result == "test line"
+
+
+def test_throwsExceptionWithBadFile(monkeypatch):
+    mock_file = MagicMock()
+    mock_file.readline = MagicMock(return_value="test line")
+    mock_open = MagicMock(return_value=mock_file)
+    monkeypatch.setattr("builtins.open", mock_open)
+    mock_exists = MagicMock(return_value=False)
+    monkeypatch.setattr("os.path.exists",mock_exists)
+    with raises(Exception):
+        result = readFromFile("check")
